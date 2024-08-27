c80e94be03d7bdef116a52424e6013412a3fbf99
FelipeMartinsFrudeli <felipem.frudeli.dev@gmail.com>
2024-08-27 00:55:22 -0300

    typeorm module import & .env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        url: config.get('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: true, // Only use in development environment!
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
